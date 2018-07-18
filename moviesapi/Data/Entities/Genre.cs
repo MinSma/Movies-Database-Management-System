87cd54d4be6f534ab0741abceb8d59af975a0bc7
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-18 23:53:58 +0300

    Add entities

---
+++ moviesapi/Data/Entities/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace movieapi.Data.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
---
+++ moviesapi/Data/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace movieapi.Data.Entities
+{
+    public class Movie
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public List<Role> Roles { get; set; }
+    }
+}
