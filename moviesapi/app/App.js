93cfeff18ba193ecb4f1b39e6a062a48589bad9d
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-18 20:54:15 +0300

    Add primary project's structure

---
+++ moviesapi/app/App.js
@@ -0,0 +1,16 @@
+import { hot } from 'react-hot-loader';
+import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import store, {history} from './store';
+import MainPage from './components/MainPage/MainPage';
+
+const App = () => (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={MainPage} />
+        </Router>
+    </Provider>
+);
+
+export default hot(module)(App);
