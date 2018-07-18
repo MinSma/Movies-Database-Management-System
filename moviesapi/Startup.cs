3d2380b68ca49e9007a6759522f1ed5040023602
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-18 23:54:46 +0300

    Add AppDBContext and add database usage in Startup

--- moviesapi/Startup.cs
+++ moviesapi/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using movieapi.Data;
 
 namespace reactdotnet
 {
@@ -18,6 +20,9 @@ namespace reactdotnet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<AppDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
