1eb8aed8c749c58bee31885b65cea8d717d92f7e
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 20:51:24 +0300

    Change router to BrowserRouter

--- moviesapi/app/App.js
+++ moviesapi/app/App.js
@@ -1,15 +1,17 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
-import store, {history} from './store';
-import MainPage from './components/MainPage/MainPage';
+import store from './store';
+import MainPage from './components/MainPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => (
     <Provider store={store}>
-        <Router history={history}>
-            <Route path="/" component={MainPage} />
-        </Router>
+        <BrowserRouter>
+            <Switch>
+                <Route path={'/'} component={MainPage} />
+            </Switch>
+        </BrowserRouter>
     </Provider>
 );
 
