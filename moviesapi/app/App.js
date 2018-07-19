aaac7033d422e4118223a28f5987126355140964
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 22:16:09 +0300

    Fix routes

--- moviesapi/app/App.js
+++ moviesapi/app/App.js
@@ -2,13 +2,17 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import MainPage from './components/MainPage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import ActorsPage from './components/ActorsPage';
+import GenresPage from './components/GenresPage';
 
 const App = () => (
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
+                <Route path={'/actors'} component={ActorsPage} />
+                <Route path={'/genres'} component={GenresPage} />
                 <Route path={'/'} component={MainPage} />
             </Switch>
         </BrowserRouter>
