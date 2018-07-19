85897ac58c01b6300220c0f96c877875e591d80d
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 19:26:45 +0300

    Create MovieCreateUpdateRequest

---
+++ moviesapi/DataContracts/Requests/MovieCreateUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace movieapi.DataContracts.Requests
+{
+    public class MovieCreateUpdateRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
