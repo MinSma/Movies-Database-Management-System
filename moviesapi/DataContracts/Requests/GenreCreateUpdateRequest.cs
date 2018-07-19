7c142a0ddff1c59fac1a2955086bef9ef6d0a6f5
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 18:51:05 +0300

    Create GenreCreateUpdateRequest

---
+++ moviesapi/DataContracts/Requests/GenreCreateUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movieapi.DataContracts.Requests
+{
+    public class GenreCreateUpdateRequest
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
