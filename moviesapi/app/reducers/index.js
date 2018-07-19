892a393831ed95ad1a9e9e6e7c67fea84bdd50db
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 22:14:49 +0300

    Add reducers

--- moviesapi/app/reducers/index.js
+++ moviesapi/app/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-const rootReducer = combineReducers({routing: routerReducer});
+import actors from './actors';
+import genres from './genres';
+import movies from './movies';
+
+const rootReducer = combineReducers({ actors, genres, movies, routing: routerReducer });
 
 export default rootReducer;
