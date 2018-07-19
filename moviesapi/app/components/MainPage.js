787c125cb4eedb7066a3203c91490cfdd4605b87
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-19 20:51:05 +0300

    Add NavigationBar Component

--- moviesapi/app/components/MainPage.js
+++ moviesapi/app/components/MainPage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import NavigationBar from "./NavigationBar";
 
 export default class MainPage extends React.Component {
     render () {
         return (
             <div>
-                HomePage
+                <NavigationBar />
             </div>
         );
     }
