43ed234615e9822143e4df25caaff6b249d13ef1
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-20 20:13:37 +0300

    Add MovieDialogForm usage in Mainpage

--- moviesapi/app/components/MainPage.js
+++ moviesapi/app/components/MainPage.js
@@ -3,20 +3,56 @@ import dateFormat from 'dateformat';
 import NavigationBar from "./NavigationBar";
 import TableComponent from "./TableComponent";
 import AddButton from './AddButton';
+import MovieDialogForm from './MovieDialogForm/MovieDialogForm';
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as actionCreators from '../actions';
 
 class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dialogIsOpen: false
+        }
+
+        this.handleButtonOnClick = this.handleButtonOnClick.bind(this);
+        this.handleDialogClose = this.handleDialogClose.bind(this);
+        this.handleDialogSubmit = this.handleDialogSubmit.bind(this);
+    }
+
     componentDidMount() {
         this.props.getAllMovies();
+        this.props.getAllGenres();
+    }
+
+    handleDialogClose() {
+        this.setState({
+            dialogIsOpen: false
+        });
+    }
+
+    handleButtonOnClick() {
+        this.setState({
+            dialogIsOpen: true
+        });
+    }
+
+    handleDialogSubmit(values) {
+        let movie = {
+            title: values.title,
+            releaseDate: `${values.year}-${values.month}-${values.day}`,
+            genreId: values.genreId
+        };
+
+        this.props.addMovie(movie);
     }
 
     render () {
         var headers = [ 
             'Title',
-            'ReleaseDate',
+            'Release Date',
             'Genre',
             'Actions'
         ];
@@ -24,7 +60,7 @@ class MainPage extends React.Component {
         var data = this.props.movies.map((movie) => {
             return [ 
                 movie.title, 
-                dateFormat(movie.releaseDate, "yyyy/mm/dd"),
+                dateFormat(movie.releaseDate, "yyyy-mm-dd"),
                 movie.genreName
             ];
         });
@@ -32,8 +68,15 @@ class MainPage extends React.Component {
         return (
             <div>
                 <NavigationBar />
-                <AddButton text={"Movie"} />
+                <AddButton action={this.handleButtonOnClick} text={"Movie"} />
                 <TableComponent headers={headers} data={data} />
+
+                {this.state.dialogIsOpen && <MovieDialogForm onSubmit={this.handleDialogSubmit}
+                                                             formTitle={"ADD NEW MOVIE"} 
+                                                             buttonText={"ADD"}
+                                                             handleClose={this.handleDialogClose} 
+                                                             genres={this.props.genres} />
+                }
             </div>
         );
     }
@@ -45,7 +88,8 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (store) => {
     return {
-        movies: store.movies
+        movies: store.movies,
+        genres: store.genres
     }
 }
 
