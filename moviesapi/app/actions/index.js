932050ef840d44c177d3e4a52cb1ef5df746ac8f
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-20 21:01:49 +0300

    Add remove/edit for GenresPage

--- moviesapi/app/actions/index.js
+++ moviesapi/app/actions/index.js
@@ -1,5 +1,7 @@
 const GET_ALL_GENRES = 'GET_ALL_GENRES';
 const ADD_GENRE = 'ADD_GENRE';
+const EDIT_GENRE = 'EDIT_GENRE';
+const REMOVE_GENRE = 'REMOVE_GENRE';
 const GET_ALL_ACTORS = 'GET_ALL_ACTORS';
 const ADD_ACTOR = 'ADD_ACTOR';
 const GET_ALL_MOVIES = 'GET_ALL_MOVIES';
@@ -44,6 +46,38 @@ export const addGenre = (values) => (dispatch) => {
             });
     }).catch((error) => console.log(error));
 };
+
+export const editGenre = (genre) => (dispatch) => {
+    fetch(`${url}/api/genres/${genre.id}`, {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify(genre),
+        credentials: 'include'
+    }).then(response => {
+        response.json()
+            .then(data => {
+                dispatch({
+                    type: EDIT_GENRE,
+                    payload: data
+                });
+            });
+    }).catch((error) => console.log(error));
+}
+
+export const removeGenre = (id) => (dispatch) => {
+    fetch(`${url}/api/genres/${id}`, {
+        method: 'DELETE',
+        headers,
+        credentials: 'include'
+    }).then(response => {
+        if(response.status === 200 || response.status === 204) {
+            dispatch({
+                type: REMOVE_GENRE,
+                id: id
+            });
+        };
+    }).catch((error) => console.log(error));
+}
 
 export const getAllActors = () => (dispatch) => {
     fetch(`${url}/api/actors`, {
