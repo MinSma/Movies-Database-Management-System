9d7e74ed02e9d32a1820ff6341543717475423b8
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-20 20:13:27 +0300

    Add MovieDialogForm and validation

---
+++ moviesapi/app/components/MovieDialogForm/MovieDialogForm.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, IconButton, Select } from '@material-ui/core';
+import { Close as CloseIcon } from '@material-ui/icons';
+import { Button, Input } from '@material-ui/core';
+import { Field, reduxForm } from 'redux-form';
+import { submitValidation } from './validation';
+import dateFormat from 'dateformat';
+
+let initialValues = {};
+
+class MovieDialogForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: true,
+            genreId: ''
+        }
+
+        if(Object.keys(this.props.initialValues).length === 0) {
+            initialValues.title = "";
+            initialValues.year = "";
+            initialValues.month = "";
+            initialValues.day = "";
+        } else {
+            initialValues.title = this.props.initialValues.title;
+            initialValues.year = dateFormat(this.props.initialValues.releaseDate, "yyyy");
+            initialValues.month = dateFormat(this.props.initialValues.releaseDate, "mm");
+            initialValues.day = dateFormat(this.props.initialValues.releaseDate, "dd");
+            
+            this.setState({
+                genreId: this.props.initialValues.genreId
+            });
+        }
+
+        this.handleClose = this.handleClose.bind(this);
+        this.submitValidation = submitValidation.bind(this);
+    }
+
+    handleClose() {
+        this.setState({
+            isOpen: false
+        });
+
+        if(this.props.handleClose !== undefined) {
+            this.props.handleClose();
+        }
+    }
+
+    handleGenreSelection(e) {
+        this.setState({
+            genreId: e.target.value
+        });
+    }
+
+    render() {
+        return ( 
+            <Dialog open={this.state.isOpen}
+                    onClose={this.handleClose}
+                    fullWidth={true}>
+                <form onSubmit={this.props.handleSubmit(this.submitValidation)}>
+                    <DialogTitle>
+                        <span className="form-title">{this.props.formTitle}</span>
+                        <IconButton className="exit-button" onClick={this.handleClose}>
+                            <CloseIcon style={{fontSize: "34px"}}/>
+                        </IconButton>
+                    </DialogTitle>
+                    <DialogContent>
+                        <div className="form-content">
+                            <div className="error-message">{this.props.error && <strong>{this.props.error}</strong>}</div>
+                            <p className="required-field">Title:</p>
+                            <Field
+                                name="title"
+                                component={this.renderInputBox}
+                                label="Title"
+                                style={this.renderInputStyles()}
+                                inputText={initialValues.title}
+                            />
+                            <p className="required-field">Release Date:</p>
+                            <span className="dateInputs">
+                                <Field
+                                    name="year"
+                                    component={this.renderInputBox}
+                                    label="Year"
+                                    placeholder="Year"
+                                    style={this.renderDateInputStyles()}
+                                    inputText={initialValues.year}
+                                />
+                            </span>
+                            <span className="dateInputs">
+                                <Field
+                                    name="month"
+                                    component={this.renderInputBox}
+                                    label="Month"
+                                    placeholder="Month"
+                                    style={this.renderDateInputStyles()}
+                                    inputText={initialValues.month}
+                                />
+                            </span>
+                            <span className="dateInputs">
+                            <Field
+                                name="day"
+                                component={this.renderInputBox}
+                                label="Day"
+                                placeholder="Day"
+                                style={this.renderDateInputStyles()}
+                                inputText={initialValues.day}
+                            />
+                            </span>
+                            <p className="required-field">Select Genre:</p>
+                            {this.renderGenreSelection()}
+                        </div>
+                        <Button type="submit" variant="raised" color="primary"
+                                style={{
+                                    width: "140px", 
+                                    height: "40px", 
+                                    margin: "5% 2%",
+                                    float: "left",
+                                    margin: "2%"
+                                }}>
+                            {this.props.buttonText}
+                        </Button>
+                        <Button variant="raised"
+                                style={{
+                                    width: "70px", 
+                                    height: "40px", 
+                                    margin: "5% 2%",
+                                    fontSize: "12px",
+                                    float: "left",
+                                    margin: "2%"
+                                }}
+                                onClick={this.handleClose}>
+                                    <CloseIcon style={{fontSize: "12px"}}/>
+                                    Close 
+                        </Button>
+                    </DialogContent>
+                </form>
+            </Dialog>
+        );
+    }
+
+    renderInputStyles() {
+        return {
+            border: "1px solid #DADFE1",
+            borderRadius: "5px",
+            height: "40px",
+            width: "91.5%",
+            paddingLeft: "10px"
+        };
+    }
+
+    renderDateInputStyles() {
+        return {
+            border: "1px solid #DADFE1",
+            width: '80%'
+        }
+    }
+
+    renderInputBox(props) {
+        delete props.input.value;
+        return(
+            <div>
+                <Input
+                    style={props.style}
+                    className={props.className}
+                    {...props.input}
+                    placeholder={props.placeholder}
+                    error={props.meta.invalid}
+                    defaultValue={props.inputText}
+                />
+                {props.touched && props.error && <span>{props.error}</span>}
+            </div>
+        );
+    }
+
+    renderDropdown(props) {
+        let selections = props.options.map(opt => {
+            return <option value={opt.id} key={opt.id}>{opt.value}</option>;
+        });
+
+        if (props.primarySelection === true) {
+            selections.unshift(<option value='' key={0}>Select {props.label}</option>)
+        }
+
+        delete props.input.value;
+        return (
+            <Select
+                native
+                style={{
+                    width: props.width,
+                    backgroundColor: "white",
+                    border: "1px solid #DADFE1",
+                    borderRadius: "5px"
+                }}
+                className="inputDropdown"
+                {...props.input}
+                {...props.custom}
+                value={props.inputText}
+                disabled={props.disabled}
+            >
+                {selections}
+            </Select>
+        );
+    }
+
+    renderGenreSelection() {
+        return (
+            <Field
+                name="genreId"
+                label="Genre"
+                options={this.props.genres.map(x => ({ id: x.id, value: x.name }))}
+                width="40%"
+                inputText={this.state.genreId}
+                component={this.renderDropdown}
+                primarySelection={true}
+                onChange={(event) => this.handleGenreSelection(event)}
+            />
+        );
+    }
+}
+
+export default reduxForm({ 
+    form: 'GenreDialogForm',
+    initialValues: initialValues
+})(MovieDialogForm);
---
+++ moviesapi/app/components/MovieDialogForm/validation.js
@@ -0,0 +1,77 @@
+import { SubmissionError } from 'redux-form';
+
+export function submitValidation(values) {
+    if(!values['title']) {
+        throw new SubmissionError({ 
+            title: "Title value required.", 
+            _error: "Title value required." 
+        });
+    }
+
+    if(!values['year']) {
+        throw new SubmissionError({
+            year: "Release year required.",
+            _error: "Release year required."
+        });
+    }
+
+    let yearRegex = /^(19|20)\d{2}$/;
+
+    if(values['year'] && values['year'].match(yearRegex) === null) {
+        throw new SubmissionError({ 
+            year: "Wrong day type value.", 
+            _error: "Wrong day type value." });
+    }
+
+    if(!values['month']) {
+        throw new SubmissionError({
+            month: "Release month required.",
+            _error: "Release month required."
+        });
+    }
+
+    let monthRegex = /^\d{2}$/;
+
+    if(values['month'] && values['month'].match(monthRegex) === null) {
+        throw new SubmissionError({ 
+            year: "Wrong month type value.", 
+            _error: "Wrong month type value." });
+    } else {
+        if(values['month'] > 12 || values['month'] < 1) {
+            throw new SubmissionError({ 
+                year: "Wrong month type value.", 
+                _error: "Wrong month type value." });
+        }
+    }
+
+    if(!values['day']) {
+        throw new SubmissionError({
+            day: "Release day required.",
+            _error: "Release day required."
+        });
+    }
+
+    let dayRegex = /^\d{2}$/;
+
+    if(values['day'] && values['day'].match(dayRegex) === null) {
+        throw new SubmissionError({ 
+            year: "Wrong day type value.", 
+            _error: "Wrong day type value." });
+    } else {
+        if(values['day'] > 31 || values['day'] < 1) {
+            throw new SubmissionError({ 
+                year: "Wrong day type value.", 
+                _error: "Wrong day type value." });
+        }
+    }
+
+    if(!values['genreId']) {
+        throw new SubmissionError({
+            genreId: "Genre value required.",
+            _error: "Genre value required."
+        });
+    }
+
+    this.props.onSubmit(values);
+    this.handleClose();
+}
