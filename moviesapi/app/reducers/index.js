8ece5d06cb8efc7b912df89c6db4b2ebaf9a5248
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-20 16:01:38 +0300

    Add redux-form reducer to rootReducer

--- moviesapi/app/reducers/index.js
+++ moviesapi/app/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as form } from 'redux-form';
 
 import actors from './actors';
 import genres from './genres';
 import movies from './movies';
 
-const rootReducer = combineReducers({ actors, genres, movies, routing: routerReducer });
+const rootReducer = combineReducers({ actors, genres, movies, form, routing: routerReducer });
 
 export default rootReducer;
