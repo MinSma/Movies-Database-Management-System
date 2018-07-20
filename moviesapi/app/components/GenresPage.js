2261f92b6fa5e103cf775734fa643e730bcc539d
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-20 16:01:22 +0300

    Use GenreDialogForm in GenresPage

--- moviesapi/app/components/GenresPage.js
+++ moviesapi/app/components/GenresPage.js
@@ -2,14 +2,43 @@ import React from 'react';
 import NavigationBar from './NavigationBar';
 import TableComponent from "./TableComponent";
 import AddButton from './AddButton';
+import GenreDialogForm from './GenreDialogForm/GenreDialogForm';
+import './styles.css';
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as actionCreators from '../actions';
 
 class GenresPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dialogIsOpen: false
+        }
+
+        this.handleButtonOnClick = this.handleButtonOnClick.bind(this);
+        this.handleDialogClose = this.handleDialogClose.bind(this);
+    }
+
     componentDidMount() {
         this.props.getAllGenres();
+    }
+
+    handleDialogClose() {
+        this.setState({
+            dialogIsOpen: false
+        });
+    }
+
+    handleButtonOnClick() {
+        this.setState({
+            dialogIsOpen: true
+        });
+    }
+
+    handleDialogSubmit(values) {
+        this.props.addGenre(values.name);
     }
 
     render() {
@@ -27,8 +56,14 @@ class GenresPage extends React.Component {
         return (
             <div>
                 <NavigationBar />
-                <AddButton text={"Genre"} />
+                <AddButton action={this.handleButtonOnClick} text={"Genre"} />
                 <TableComponent headers={headers} data={data} />
+
+                {this.state.dialogIsOpen && <GenreDialogForm onSubmit={this.handleDialogSubmit}
+                                                             formTitle={"ADD NEW GENRE"} 
+                                                             buttonText={"ADD"}
+                                                             handleClose={this.handleDialogClose} />
+                }
             </div>
         );
     }
