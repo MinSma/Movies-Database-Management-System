01bfac20461639d9d6de29d16fe001600ae56690
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-22 20:51:03 +0300

    Add actors adding render

--- moviesapi/app/components/renderFunctionsForDialogs.js
+++ moviesapi/app/components/renderFunctionsForDialogs.js
@@ -1,5 +1,33 @@
 import React from 'react';
-import { Input } from '@material-ui/core';
+import { Input, Button } from '@material-ui/core';
+import { Field } from 'redux-form';
+
+export const renderActors = ({ fields, meta: { touched, error, submitFailed } }) => (
+    <ul>
+        {fields.map((actor, index) => (
+            <li key={index}>
+                <h4 style={{margin: '1% 12% 1% 1%'}}>Actor #{index + 1}</h4>
+                <Field
+                    name={`${actor}.firstName`}
+                    component={renderInputBox}
+                    style={renderInputStyles()}
+                    placeholder="First Name"
+                />
+                <Field
+                    name={`${actor}.lastName`}
+                    component={renderInputBox}
+                    style={renderInputStyles()}
+                    placeholder="Last Name"
+                />
+                <Button onClick={() => fields.remove(index)} variant="raised" color="secondary" style={{margin: '1% 12% 1% 1%'}}>Remove</Button>
+            </li>
+        ))}
+        <li>
+            <Button onClick={() => fields.push({})} variant="raised" color="primary" style={{margin: '1% 12% 1% 1%'}}>Add Actor</Button>
+            {(touched || submitFailed) && error && <span>{error}</span>}
+        </li>
+    </ul>
+  );
 
 export const renderInputBox = (props) => {
     delete props.input.value;
@@ -8,6 +36,7 @@ export const renderInputBox = (props) => {
             {props.meta.touched && props.meta.error && <span className="error-message">{props.meta.error}</span>}
             <Input
                 style={props.style}
+                placeholder={props.placeholder}
                 className={props.className}
                 {...props.input}
                 error={props.meta.invalid}
