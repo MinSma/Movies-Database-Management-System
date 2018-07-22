cda5877bd5b70f893a1f6c9d201ed2af38470e0c
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-22 12:19:32 +0300

    Add GenreDialog type validation and fix type validation error show

--- moviesapi/app/components/GenreDialogForm/validation.js
+++ moviesapi/app/components/GenreDialogForm/validation.js
@@ -1,5 +1,17 @@
 import { SubmissionError } from 'redux-form';
 
+export function typeValidation(values) {
+    let errors = {};
+
+    let nameRegex = /^[A-Za-z]*$/;
+
+    if(values['name'] !== undefined && !values['name'].match(nameRegex)) {
+        errors.name = "Wrong name format";
+    }
+
+    return errors;
+}
+
 export function submitValidation(values) {
     if(!values['name']) {
         throw new SubmissionError({ title: "Name value required.", 
