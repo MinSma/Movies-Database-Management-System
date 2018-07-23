71a1c2a62b0cf01c2a8ef156bb184bb97cbe01eb
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-07-23 19:12:39 +0300

    Add CreateDeleteRalationshipRequest

---
+++ moviesapi/DataContracts/Requests/CreateDeleteRelationshipRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieapi.DataContracts.Requests
+{
+    public class CreateDeleteRelationshipRequest
+    {
+        public int MovieId { get; set; }
+
+        public int ActorId { get; set; }
+    }
+}
